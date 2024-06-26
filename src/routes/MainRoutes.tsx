ab7a4286e964e7959c9f07d0b9357460c8d201f7
muhammad19omer <mohd2000omer@gmail.com>
2024-06-26 16:16:31 +0500

    removed unnecessary files

--- src/routes/MainRoutes.tsx
+++ src/routes/MainRoutes.tsx
@@ -48,18 +48,6 @@ const MaintenanceError = Loadable(
 const MaintenanceError500 = Loadable(
   lazy(() => import("pages/maintenance/error/500"))
 );
-const MaintenanceUnderConstruction = Loadable(
-  lazy(() => import("pages/maintenance/under-construction/under-construction"))
-);
-const MaintenanceComingSoon = Loadable(
-  lazy(() => import("pages/maintenance/coming-soon/coming-soon"))
-);
-
-const AppContactUS = Loadable(lazy(() => import("pages/contact-us")));
-// render - sample page
-const SamplePage = Loadable(
-  lazy(() => import("pages/extra-pages/sample-page"))
-);
 
 // ==============================|| MAIN ROUTES ||============================== //
 
