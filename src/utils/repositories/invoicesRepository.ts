b21969341ff155016d9f19b36fb357461272b41f
muhammad19omer <mohd2000omer@gmail.com>
2024-06-30 04:14:05 +0500

    added support for two commissions for dual user

--- src/utils/repositories/invoicesRepository.ts
+++ src/utils/repositories/invoicesRepository.ts
@@ -17,30 +17,30 @@ class InvoicesRepository {
 
   public async create(invoice: InvoiceSupabase) {
     try {
-      // const { data: existingInvoiceData, error: existingInvoiceError } =
-      //   await supabase
-      //     .from(this.className)
-      //     .select()
-      //     .eq("sale", invoice.sale)
-      //     .eq("beneficiary", invoice.beneficiary);
+      const { data: existingInvoiceData, error: existingInvoiceError } =
+        await supabase
+          .from(this.className)
+          .select()
+          .eq("sale", invoice.sale)
+          .eq("beneficiary", invoice.beneficiary);
 
-      // if (
-      //   existingInvoiceData &&
-      //   existingInvoiceData.length > 0 &&
-      //   !existingInvoiceError
-      // ) {
-      //   return existingInvoiceData[0];
-      // } else {
-      const { data, error } = await supabase
-        .from(this.className)
-        .insert(invoice)
-        .select();
+      if (
+        existingInvoiceData &&
+        existingInvoiceData.length >= 2 &&
+        !existingInvoiceError
+      ) {
+        return existingInvoiceData[0];
+      } else {
+        const { data, error } = await supabase
+          .from(this.className)
+          .insert(invoice)
+          .select();
 
-      if (data && data.length > 0 && error === null) {
-        return data[0];
+        if (data && data.length > 0 && error === null) {
+          return data[0];
+        }
+        return null;
       }
-      return null;
-      // }
     } catch (error) {
       console.error("Error creating new invoice:", error);
       return null;
