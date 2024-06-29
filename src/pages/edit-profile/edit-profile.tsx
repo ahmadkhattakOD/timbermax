af65c1d749f5e7906d0c4cc0ea3553cac3712afa
muhammad19omer <mohd2000omer@gmail.com>
2024-06-30 03:19:57 +0500

    added username support, removed emails

--- src/pages/edit-profile/edit-profile.tsx
+++ src/pages/edit-profile/edit-profile.tsx
@@ -6,7 +6,7 @@ import FormInput from "components/FormInput";
 import FormDropdown from "components/FormDropdown";
 import { useEditProfile } from "./useEditProfile";
 import CircularLoader from "components/CircularLoader";
-import { userRoles } from "utils/helpers";
+import { stripEmail, userRoles } from "utils/helpers";
 
 import ChangePassword from "components/change-password/ChangePassword";
 
@@ -36,7 +36,7 @@ export default function EditProfile() {
         enableReinitialize
         initialValues={{
           fullName: profile.full_name ?? "",
-          email: profile.email ?? "",
+          email: stripEmail(profile.email) ?? "",
           role: profile.role ?? "",
           commission:
             profile.commission !== null ? profile.commission.toString() : "",
@@ -65,10 +65,10 @@ export default function EditProfile() {
                 <FormInput
                   id={"email"}
                   name={"email"}
-                  placeholder={"Email"}
-                  label={"email"}
+                  placeholder={"Username"}
+                  label={"username"}
                   optional={false}
-                  type={"email"}
+                  type={"text"}
                   disabled
                 />,
                 <FormInput
